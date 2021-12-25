85c8624d110c654107fb6441e1e2b4724abcfe67
Andrés Muñoz <andres.munoz@idukay.com>
2021-12-25 04:15:40 -0500

    custom first basic function

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,14 +5,20 @@ import "hardhat/console.sol";
 
 contract Greeter {
     string private greeting;
+    string private name;
 
-    constructor(string memory _greeting) {
+    constructor(string memory _greeting, string memory _name) {
         console.log("Deploying a Greeter with greeting:", _greeting);
         greeting = _greeting;
+        name = _name;
     }
 
     function greet() public view returns (string memory) {
         return greeting;
+    }
+
+    function greetName() public view returns (string memory) {
+        return string(abi.encodePacked("Hello, ", name, "!"));
     }
 
     function setGreeting(string memory _greeting) public {
