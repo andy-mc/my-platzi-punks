405b2d46e59b6cbc151ca1e49cac53ba6fb11729
Andrés Muñoz <andres.munoz@idukay.com>
2021-12-26 21:15:42 -0500

    implement tokenURI ERC721

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./Base64.sol";
 
 contract PlatziPunks is ERC721, ERC721Enumerable {
   using Counters for Counters.Counter;
@@ -21,6 +23,27 @@ contract PlatziPunks is ERC721, ERC721Enumerable {
 
     _safeMint(msg.sender, tokenId);
     _tokenCounter.increment();
+  }
+  
+  function tokenURI(uint256 tokenId)
+    public 
+    view 
+    override 
+    returns (string memory) 
+  {
+    require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    
+    string memory tokenIdString = Strings.toString(tokenId);
+
+    string memory jsonUri = Base64.encode(
+      abi.encodePacked(
+        '{ "name": "PlatziPunks #', tokenIdString, '", ',
+        '"description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", ',
+        '"image": "// TODO imageUrl" }'
+      )
+    );
+    
+    return string(abi.encodePacked("data:application/json;base64,", jsonUri));
   }
 
   // The following functions are overrides required by Solidity to support ERC721Enumerable ext.
--- test/PlatziPunks-test.js
+++ test/PlatziPunks-test.js
@@ -46,10 +46,36 @@ describe("PlatziPunks", function () {
         await platzi_punks.mint()
         await platzi_punks.mint()
         await platzi_punks.mint()
-        expect.fail('should throw a No Platzi Punks left :( error');
+        expect.fail('fail with an error');
       } catch (error) {
         expect(error.message).to.contains('No Platzi Punks left :(');
       }
     });
   }) 
+
+  describe("tokenURI", async function () {
+    it("Should throw an error if tokenId don't exists", async function () {
+      try {
+        await platzi_punks.tokenURI(0)
+        expect.fail('fail with an error');
+      } catch (error) {
+        expect(error.message).to.contains('ERC721Metadata: URI query for nonexistent token');
+      }
+    });
+    
+    it("Should return the correct tokenURI protocol mime type", async function () {
+        await platzi_punks.mint()
+        expect(await platzi_punks.tokenURI(0)).to.includes("data:application/json;base64,");
+    });
+
+    it.only("Should name token with correct number based on tokenId", async function () {
+      await platzi_punks.mint()
+      await platzi_punks.mint()
+      
+      console.log('platzi_punks.tokenURI(0):', await platzi_punks.tokenURI(0))
+      expect(await platzi_punks.tokenURI(0)).to.includes(Buffer.from('{ "name": "PlatziPunks #0"').toString('base64').slice(0, -1));
+      expect(await platzi_punks.tokenURI(1)).to.includes(Buffer.from('{ "name": "PlatziPunks #1"').toString('base64').slice(0, -1));
+    });
+  })
+  
 });
